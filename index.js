13489f140a8c20504f89e63b3acf8395e2c00eaa
leonidasmaterial <andreleonedas@gmail.com>
2022-08-31 10:11:52 -0300

    primeira aula

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log("aula");
+console.log('aula');
+console.log(`aula`);
+console.log("125");
+//string
+console.log(12345);
+console.log(12.345);
+//number
+
+alert("caixa que vai aparecer no navegador");
+
+//cometario
+/*
+cometario longo falando sobre variaveis
+var
+let
+const
+
+*Não podemos criar variaveis com palavras reservadas;
+*O nome da variavel deve ter um significado;
+*Nomes de var com mais de uma palavra a segunda deve 
+iniciar com maiuscula nomeAluno, nomePaiDoAluno;
+*Não deve começar uma var com numeros;
+*Não deve ter espaço ou traços; 
+*É case-sensitive;
+*/
+var nome="roberto";
+let idade=15;
+const dataNascimento= 12;
+console.log(`meu nome é ${nome} tenho ${idade} anos`);
+console.log('ola eu sou o '+ nome+ ' tenho '+idade+' anos')
